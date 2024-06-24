98f0c69b5c1889a0a7a5acaa062812dd732a2bb9
Leandro Matamoros <127903058+LeandroEsteban@users.noreply.github.com>
2024-06-24 19:07:22 -0400

    Update DatabaseConnection.java

--- src/main/java/DatabaseConnection.java
+++ src/main/java/DatabaseConnection.java
@@ -5,7 +5,7 @@ import java.sql.SQLException;
 public class DatabaseConnection {
     private static final String URL = "jdbc:postgresql://localhost:5432/Cine";
     private static final String USER = "postgres";
-    private static final String PASSWORD = ".l3o53485.X%"; // Reemplaza con tu contraseña
+    private static final String PASSWORD = ""; // Reemplaza con tu contraseña
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
